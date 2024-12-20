8300c8176a7ba3bb73fbd5b6ab03f93fcf8c9dd3
CyberWarrior404 <mdmor@icloud.com>
2024-12-20 17:02:21 +0400

    Add files via upload

---
+++ Sets.py
@@ -0,0 +1,23 @@
+foods= {"burger","fries","pizza","salad","rice","dumplings","apple",}
+print(foods)
+
+print(type(foods))
+
+
+foods.add("Lopado_temacho_selacho_galeo_kranio_leipsano_drim_hyp_trimmato_silphio_karabo_melit_katakechy_meno_kichl_epi_kossypho_phatto_periste_alektryo_opte_kephallio_kigklo_peleio_lagoio_siraio_baphe_tragano_pterygon")
+print(foods)
+
+foods.remove("apple")
+print(foods)
+
+foods.pop
+print(foods)
+
+A = {"HA","Y"}
+B = {"PP","Y"}
+
+print(A.union(B))
+print(A.difference(B))
+print(B.difference(A))
+print(A.intersection(B))
+print(A.symmetric_difference(B))
