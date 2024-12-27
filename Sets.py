1efc3a5a5e6670c426f3a09315a301131f7f1513
CyberWarrior404 <mdmor@icloud.com>
2024-12-27 16:35:43 +0400

    Add files via upload

--- Sets.py
+++ Sets.py
@@ -21,3 +21,26 @@ print(A.difference(B))
 print(B.difference(A))
 print(A.intersection(B))
 print(A.symmetric_difference(B))
+#Game
+
+
+print("Choose on of the folowing options:")
+print("1.Create a Union")
+print("2.Create a difference")
+print("3.Create a intersection")
+print("4.Create a symetric difference")
+
+while True:
+    UserChoice = input("Choose on of the folowing numbers:")
+    if UserChoice == "1":
+        print(A.union(B))
+    elif UserChoice =="2":
+        print(A.difference(B))
+    elif UserChoice =="3":
+        print(A.intersection(B))
+    elif UserChoice == "4":
+        print(A.symmetric_difference(B))
+    else:
+        print("???(type 1,2,3 or 4)")
+
+
